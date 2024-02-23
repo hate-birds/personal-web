b2355029fab82da959965519b2e87acc94acbdfe
Молчанов Дмитрий Сергеевич <dsmolchanov@edu.hse.ru>
2024-02-23 19:43:15 +0000

    main with professional addition

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     with open('resume.txt', 'w') as f:
         f.write('Имя: Винт')
-
+        f.write('Текущая должность: инженер эксплуатации')
 
 if __name__ == "__main__":
     main()
